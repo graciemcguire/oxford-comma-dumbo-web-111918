adc6be0fc71db01ebda5c5411e52d48f8044c618
Gracie McGuire <m.graciemcguire@gmail.com>
2018-10-17 20:52:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,27 @@
 def oxford_comma(array)
   if array.length == 1
-    array.join
+    return "#{array[0]}"
   elsif array.length == 2 
     array.join(" and ")
-  elsif array.length > 2
-  array.insert(" and ", 2)
-  array.join(", ")
+  elsif array.length >= 3
+  last = "and #{array[-1]}"
+  array.pop
+  array.push(last_array_item)
+  return array.join(", ")
+  end
+end
+
+
+
+def oxford_comma(array)
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    new_last_array_item = "and #{array[-1]}"
+    array.pop
+    array.push(new_last_array_item)
+    return array.join(", ")
   end
 end
